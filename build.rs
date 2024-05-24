d8e8f513cfe007a6243a7eef571677fd366d2cb9
Markus Ast <m@rkusa.st>
2024-05-24 11:52:40 +0200

    update to protoc v27.0

--- build.rs
+++ build.rs
@@ -24,11 +24,11 @@ fn main() {
 
 fn exectuable() -> &'static str {
     match (env::consts::OS, env::consts::ARCH) {
-        ("linux", "x86_64") => "protoc-3.21.6-linux-x86_64",
-        ("linux", "aarch64") => "protoc-3.21.6-linux-aarch_64",
-        ("macos", "x86_64") => "protoc-3.21.6-osx-x86_64",
-        ("macos", "aarch64") => "protoc-3.21.6-osx-aarch_64",
-        ("windows", "x86_64") => "protoc-3.21.6-win64.exe",
+        ("linux", "x86_64") => "protoc-27.0-linux-x86_64",
+        ("linux", "aarch64") => "protoc-27.0-linux-aarch_64",
+        ("macos", "x86_64") => "protoc-27.0-osx-x86_64",
+        ("macos", "aarch64") => "protoc-27.0-osx-aarch_64",
+        ("windows", "x86_64") => "protoc-27.0-win64.exe",
         (os, arch) => panic!(
             "protoc-bundled does not include executable for {}@{}",
             os, arch
